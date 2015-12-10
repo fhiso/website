5c749874324b604ca5ef11dc3b492d86c39fedfe
Richard Smith <rsmith@fhiso.org>
2015-12-10 01:12:20 +0000

    Allow building site in other directories.

--- update-all.sh
+++ update-all.sh
@@ -1,29 +1,38 @@
 #!/bin/sh
 
-set -e 
+set -e
 
-OUT=$HOME/www
-BUILD=$HOME/www-build
+ROOT=$HOME
+
+# To override the directory the site is built in, create a file called
+# local-settings in this directory.
+if [ -e $(dirname $0)/local-settings ]; then
+    . $(dirname $0)/local-settings
+fi
+
+OUT=$ROOT/www
+BUILD=$ROOT/www-build
 
 rm -rf $BUILD
 
-cd $HOME/lexicon-eg
+cd $ROOT/lexicon-eg
 git pull -q
 cd builder
 python mergemd.py
 ln -sf lexicon-$(TZ=UTC date +%Y-%m-%d).md snapshot.md
 
-cd $HOME/tsc-governance
+cd $ROOT/tsc-governance
 git pull -q
 
-cd $HOME/sources-and-citations-eg
+cd $ROOT/sources-and-citations-eg
 git pull -q
 
-cd $HOME/bibliography
+cd $ROOT/bibliography
 git pull -q
 
-cd $HOME/website
+cd $ROOT/website
 git pull -q
+mkdir -p $BUILD
 perl build-site.pl
 rsync -rp include/ $BUILD/include/
 rsync -rp account/ $BUILD/account/
