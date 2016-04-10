f342bab11f6f06efda2e3cf66cbdd5cec6e4b021
Richard Smith <rsmith@fhiso.org>
2016-04-10 23:00:43 +0100

    Convert to use Noto fonts

--- pandoc.mk
+++ pandoc.mk
@@ -7,15 +7,22 @@ MD_DIALECT := markdown+definition_lists+header_attributes-auto_identifiers
 # We also want definitions and YAML metadata blocks.
 MD_DIALECT := $(MD_DIALECT)+definition_lists+yaml_metadata_block
 
-%.html:	%.md
-	pandoc -f $(MD_DIALECT) -o "$@" "$^"
+PANDOC := pandoc
 
+# Allow any changes for the local machine.
+# (E.g. to set PANDOC=schroot -c testing -- pandoc.)
+-include local-settings
+
+%.html:	%.md
+	$(PANDOC) -f $(MD_DIALECT) -o "$@" "$^"
+
+# Setting fontfamily=fhiso is a bit of an abuse, as fhiso.sty does much 
+# more than just setting the font.
 PDF_OPTS=-V documentclass:article --chapters -V papersize:a4 \
-         -V fontsize:11pt -V fontfamily:fhiso -V geometry:a4paper \
-         --latex-engine=xelatex
+         -V fontsize:11pt -V fontfamily:fhiso --latex-engine=xelatex
 
 %.pdf: %.md fhiso.sty logo.png pandoc.mk
-	pandoc $(PDF_OPTS) -f $(MD_DIALECT) -o "$@" "$<"
+	$(PANDOC) $(PDF_OPTS) -f $(MD_DIALECT) -o "$@" "$<"
 
 .dialect:
 	@echo -n "$(MD_DIALECT)" > $@
