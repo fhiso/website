e1a732023ebb2a520b8e1aea03e57ad0b7e88481
Richard Smith <rsmith@fhiso.org>
2016-04-20 23:36:50 +0100

    Improved styling

--- pclasses.pl
+++ pclasses.pl
@@ -5,14 +5,16 @@ use warnings;
 
 my $newp = 1; 
 my $class;
+my $long;
 
 while (<>) {
     chomp;
-    if ($newp and s/^{\.([a-z]+)}\s*//) { 
-        print "<div class=\"fhiso-$1\">\\fhisoopenclass{$1}\n";
+    if ($newp and s/^{\.([a-z]+)(\s*\.{3})?}\s*//) { 
+        $long = $2 ? " long" : "";
+        print "<div class=\"fhiso-$1$long\">\\fhisoopenclass{$1}\n";
         $class = $1;
     }
-    if (defined $class and /^\s*$/) {
+    if (defined $class and ($long ? s/^\{\/\}\s*$// : /^\s*$/)) {
         print "\\fhisocloseclass{$class}</div>\n";
         $class = undef;
     }
