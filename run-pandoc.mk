7bf1399a7fa4824be648706b8e625299299186bd
Richard Smith <rsmith@fhiso.org>
2017-01-30 23:50:18 +0000

    Simplify build environment

--- run-pandoc.mk
+++ run-pandoc.mk
@@ -25,9 +25,42 @@ clean:
 
 .PHONY: all pdf html clean
 
-%.pdf: %.md
-	$(MAKE) -C $(ROOT)/website -f pandoc.mk $(ROOT)/$(REPO)/$@
+# Disable support for auto-identifiers and explicitly allow header attributes.
+# We don't generate identifiers from headers because we want the identifiers
+# to be stable to allow references from other documents, but we want to allow
+# the header text to be changed.  Plus the auto-identifiers end up too verbose.
+MD_DIALECT := markdown+definition_lists+header_attributes-auto_identifiers
 
-%.html: %.md
-	$(MAKE) -C $(ROOT)/website -f pandoc.mk $(ROOT)/$(REPO)/$@
+# We also want definitions and YAML metadata blocks.
+MD_DIALECT := $(MD_DIALECT)+definition_lists+yaml_metadata_block
+MD_DIALECT := $(MD_DIALECT)+shortcut_reference_links+simple_tables
+
+PANDOC := pandoc
+
+# Allow any changes for the local machine.
+# (E.g. to set PANDOC=schroot -c testing -- pandoc.)
+-include local-settings.mk
+-include $(ROOT)/website/local-settings.mk
+
+
+# This is our own markdown preprocessor
+PPMD := $(ROOT)/website/preprocess-md.pl
+
+
+%.html:	%.md $(ROOT)/website/run-pandoc.mk
+	$(PPMD) < "$<" | $(PANDOC) -f $(MD_DIALECT) -o "$@"
+
+# Setting fontfamily=fhiso is a bit of an abuse, as fhiso.sty does much 
+# more than just setting the font.
+PDF_OPTS := -V documentclass:article --chapters -V papersize:a4paper -V dir:1 \
+            -V fontsize:11pt -V fontfamily:$(ROOT)/website/fhiso \
+	    -V header-includes:\\fhisoFinal --latex-engine=xelatex --standalone
+
+PDF_DEPS := $(ROOT)/website/fhiso.sty $(ROOT)/website/logo.png
+
+%.pdf: %.md $(PDF_DEPS) $(ROOT)/website/run-pandoc.mk
+	$(PPMD) < "$<" | $(PANDOC) $(PDF_OPTS) -f $(MD_DIALECT) -o "$@"
+
+%.tex: %.md $(PDF_DEPS) $(ROOT)/website/run-pandoc.mk
+	$(PPMD) < "$<" | $(PANDOC) $(PDF_OPTS) -f $(MD_DIALECT) -o "$@" 
 
