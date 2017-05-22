54abd44b2e21a131f57dfa90ed5904f5cdc9b2a2
Richard Smith <rsmith@fhiso.org>
2017-05-22 20:53:34 +0100

    Implement {#include ...}

--- preprocess-md.pl
+++ preprocess-md.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 
+use Perl6::Slurp;
+
 my $newp = 1; 
 my $class;
 my $long;
@@ -34,6 +36,13 @@ sub text($) {
 
 while (<>) {
     chomp;
+
+    if ($newp and s/^{#include\s*(.*?)}$//) {
+        my $txt = slurp($1) or die "Unable to read file '$1'";
+        $txt =~ s/^/    /gm;
+        print "$txt\n";
+    }
+
     if ($newp and s/^{\.([a-z]+)(\s*\.{3})?}\s*//) { 
         $long = $2 ? " long" : "";
         print "<div class=\"fhiso-$1$long\">\\fhisoopenclass{$1}\n";
